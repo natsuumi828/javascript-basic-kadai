30a39fed4738871e5151ebca90493c0f684eee84
natsuumi828 <145177011+natsuumi828@users.noreply.github.com>
2023-11-25 22:32:23 +0900

    Add files via upload

--- kadai_20/event.js
+++ kadai_20/event.js
@@ -1,12 +1,3 @@
-// const btn = document.getElementById('btn');
-//   btn.addEventListener('click', () => {
-//   text.textContent="ボタンをクリックしました";
-// })
-// function fn() {
-//   console.log("ボタンをクリックしました");
-// };
-//  setTimeout(function(){fn()},2000);
-
 document.getElementById('btn').onclick =function() {
 document.getElementById('text').textContent="ボタンをクリックしました";
 console.log("ボタンをクリックしました");
