b0d911137511d67d69b4b622e8a79b6a7009351e
natsuumi828 <145177011+natsuumi828@users.noreply.github.com>
2023-11-25 12:41:01 +0900

    Add files via upload

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,7 +1,15 @@
+// const today = new Date();
+// const date = () => {
+//   console.log(today.getFullYear () +'年');
+//   console.log(today.getMonth() + 1 +'月');
+//   console.log(today.getDate() + '日');
+// }
+// date();
+
 const today = new Date();
-const date = () => {
-  console.log(today.getFullYear () +'年');
-  console.log(today.getMonth() + 1 +'月');
-  console.log(today.getDate() + '日');
-}
-date();
+
+const year = today.getFullYear();
+const month = today.getMonth() + 1;
+const day = today.getDate();
+
+console.log( year + '年' + month + '月' + day + '日');
